94811dd94ecb0926eb0ab64d61327c49898e5be9
Diego Alberto Anaya Márquez <73500149+A01379375@users.noreply.github.com>
2020-10-29 14:55:26 -0600

    Update blancoynegro.py

--- blancoynegro.py
+++ blancoynegro.py
@@ -1,4 +1,27 @@
 #
+
+import numpy as np
+import cv2
+
+def rgb2gray(A):
+    filas = len(A)
+    columnas = len(A[0])
+    B = np.zeros((filas,columnas))
+    for i in range(filas):
+        for j in range(columnas):
+            B[i][j] = (A[i][j][0]*0.07 + A[i][j][1]*0.72 + A[i][j][2]*0.21)
+    return B
+
+
+imagen_i = cv2.imread("imagen.jpg")
+imagenrgb2gray = rgb2gray(imagen_i)
+cv2.imwrite("Imagen_escala_de_grises.jpg",imagenrgb2gray)
+
+imagen_2 = cv2.imread('paisaje.jpg')
+imagen_2 = cv2.resize(imagen_2, (512, 256))
+imagen2rgb2gray = rgb2gray(imagen_2)
+cv2.imwrite('Imagen_paisaje_escala_de_grises.jpg', imagen2rgb2gray)
+
 def blancoynegro(imagen):
     valorF = len(imagen)
     valorC = len(imagen[0])
@@ -11,8 +34,8 @@ def blancoynegro(imagen):
             else:
                 bw[x][y] = 1
         return bw
-    
-imagen_i = cv2.imread("imagen_escala_de_grises.jpg")
-byn = blancoynegro(imagen_i)
-cv2.imwrite("blanco_negro.jpg", byn)
 
+byn = blancoynegro(imagenrgb2gray)
+cv2.imwrite("blanco_negro.jpg", byn)
+byn2 = blancoynegro(imagen2rgb2gray)
+cv2.imwrite("blanco_negro_paisaje.jpg", byn2)
