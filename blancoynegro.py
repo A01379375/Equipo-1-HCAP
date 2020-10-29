21531e22f4ad2b4282af4e365789a13a9ae8c7b2
Eva Denisse Vargas Sosa <65176505+A01377098@users.noreply.github.com>
2020-10-29 14:52:01 -0600

    Create blancoynegro.py
    Transformación de blanco y negro :D

---
+++ blancoynegro.py
@@ -0,0 +1,18 @@
+#
+def blancoynegro(imagen):
+    valorF = len(imagen)
+    valorC = len(imagen[0])
+    bw = np.zeros((valorF, valorC))
+    valorIntermedio = 128
+    for x in range (len(bw)):
+        for y in range (len(valorC)):
+            if imagen[x][y] <= valorIntemedio:
+                bw[x][y]  = 0
+            else:
+                bw[x][y] = 1
+        return bw
+    
+imagen_i = cv2.imread("imagen_escala_de_grises.jpg")
+byn = blancoynegro(imagen_i)
+cv2.imwrite("blanco_negro.jpg", byn)
+
