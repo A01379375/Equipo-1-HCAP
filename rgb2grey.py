2f4a083638691eb850dfefc3a57ca9b196bab883
LeonardoNavaC <73500153+LeonardoNavaC@users.noreply.github.com>
2020-10-29 14:08:17 -0600

    Add files via upload

---
+++ rgb2grey.py
@@ -0,0 +1,17 @@
+import numpy as np
+import cv2
+
+def rgb2gray(A):
+    filas = len(A)
+    columnas = len(A[0])
+    B = np.zeros((filas,columnas))
+    for i in range(filas):
+        for j in range(columnas):
+            B[i][j] = (A[i][j][0]*0.07 + A[i][j][1]*0.72 + A[i][j][2]*0.21)
+    return B
+
+
+imagen_i = cv2.imread("imagen.jpg")
+print(imagen_i.shape)
+imagenrgb2gray = rgb2gray(imagen_i)
+cv2.imwrite("Imagen_blanco_y_negro.jpg",imagenrgb2gray)
